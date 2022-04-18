fa8908cc40b75acb4d4b51ee4fb36d8943d3fbc0
zabec <denis.imsirovic@gmail.com>
2022-04-18 10:17:48 +0200

    unclear scoring error, changed array const to let

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,5 +1,5 @@
 // Only change code below this line
-const numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
+let numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
 
 let uniqueSet = new Set(numberArray.sort(function(a, b){return a - b}));
 
