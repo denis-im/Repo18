a23fd40ac3dd0d6d24e705bc56dbe3ac435e85b0
zabec <denis.imsirovic@gmail.com>
2022-04-18 10:21:45 +0200

    removed additional variable

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,9 +1,7 @@
 // Only change code below this line
 let numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
 
-let uniqueSet = new Set(numberArray.sort(function(a, b){return a - b}));
-
-let uniqueArray = Array.from(uniqueSet);
+let uniqueArray = [...new Set(numberArray.sort(function(a, b){return a - b}))];
 
 console.log(uniqueArray);
 
