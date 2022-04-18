da44ca82322955d837a00e7cba14dd4d6aca443f
zabec <denis.imsirovic@gmail.com>
2022-04-18 09:45:51 +0200

    multiplication bug

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -2,13 +2,13 @@ let myArr = [];
 // Only change code below this line
 function* multiplication(x) {
   for(let i = 0; i < x; i++) {
-    x *= x;
+    x *= 2;
     yield x;
     myArr.push(x);
   }
 }
 
-let num = multiplication(3);
+var num = multiplication(3);
 
 for (let i = 0; i < 3; i++){
   console.log(num.next());
